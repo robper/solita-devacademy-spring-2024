d5554d02dbc0bdc186f8674f3d7bd4d5a7f8995f
Robin <paaarsson@gmail.com>
2024-06-20 23:47:29 +0200

    Count endpoint for distance and duration

--- sda-backend/Program.cs
+++ sda-backend/Program.cs
@@ -39,20 +39,34 @@ app.MapGet("/stations/{id}", (int id, Context db) => db.Stations.FindAsync(id))
 .WithName("Station")
 .WithOpenApi();
 
-app.MapGet("/stations/{id}/depatures", (int id, Context db) => db.Journeys.Where(j => j.Departure_station_id == id).Take(100).ToListAsync())
+app.MapGet("/stations/{id}/depatures", (int id, Context db) => 
+    db.Journeys.Where(j => j.Departure_station_id == id).Take(100).ToListAsync())
 .WithName("Station depatures")
 .WithOpenApi();
 
-app.MapGet("/stations/{id}/depatures/distance", (int id, Context db) => db.Journeys.Where(j => j.Departure_station_id == id).Average(j => j.Distance))
+app.MapGet("/stations/{id}/depatures/count", (int id, Context db) =>
+    new { count = db.Journeys.Where(j => j.Departure_station_id == id).CountAsync().Result })
+.WithName("Nr of station depatures")
+.WithOpenApi();
+
+app.MapGet("/stations/{id}/depatures/distance", (int id, Context db) =>
+    db.Journeys.Where(j => j.Departure_station_id == id).Average(j => j.Distance))
 .WithName("Avrage depature distance")
 .WithOpenApi();
 
-app.MapGet("/stations/{id}/depatures/duration", (int id, Context db) => db.Journeys.Where(j => j.Departure_station_id == id).Average(j => j.Duration))
+app.MapGet("/stations/{id}/depatures/duration", (int id, Context db) =>
+    db.Journeys.Where(j => j.Departure_station_id == id).Average(j => j.Duration))
 .WithName("Avrage depature duration")
 .WithOpenApi();
 
-app.MapGet("/stations/{id}/returns", (int id, Context db) => db.Journeys.Where(j => j.Return_station_id == id).Take(100).ToListAsync())
+app.MapGet("/stations/{id}/returns", (int id, Context db) =>
+    db.Journeys.Where(j => j.Return_station_id == id).Take(100).ToListAsync())
 .WithName("Station returns")
+.WithOpenApi();
+
+app.MapGet("/stations/{id}/returns/count", (int id, Context db) =>
+    new { count = db.Journeys.Where(j => j.Return_station_id == id).CountAsync().Result })
+.WithName("Nr of station returns")
 .WithOpenApi();
 
 app.MapGet("/journeys", (Context db) => db.Journeys.Take(100).ToListAsync())
