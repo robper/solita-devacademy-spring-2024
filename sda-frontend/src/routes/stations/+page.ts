c23686a5e4af0ec9466845bfd79b788ca6d7aeb8
Robin <paaarsson@gmail.com>
2024-06-28 20:43:02 +0200

    Configure backend API host using .env

--- sda-frontend/src/routes/stations/+page.ts
+++ sda-frontend/src/routes/stations/+page.ts
@@ -1,5 +1,7 @@
+import { PUBLIC_BACKEND_API } from '$env/static/public';
+
 export async function load({}){
-    let response = await fetch("http://localhost:5221/stations");
+    let response = await fetch(`${PUBLIC_BACKEND_API}/stations`);
     let stations = await response.json() as Station[];
 
 	return {
--- sda-frontend/src/routes/stations/[station]/+page.ts
+++ sda-frontend/src/routes/stations/[station]/+page.ts
@@ -1,10 +1,13 @@
+import { PUBLIC_BACKEND_API } from '$env/static/public';
+
 export async function load({params}){
     console.log(params);
-    let station = await fetch(`http://localhost:5221/stations/${params.station}`);
-    let dep_count = await fetch(`http://localhost:5221/stations/${params.station}/depatures/count`);
-    let dep_dist = await fetch(`http://localhost:5221/stations/${params.station}/depatures/distance`);
-    let dep_dur = await fetch(`http://localhost:5221/stations/${params.station}/depatures/duration`);
-    let ret_count = await fetch(`http://localhost:5221/stations/${params.station}/returns/count`);
+    console.log('Backend api url: ', PUBLIC_BACKEND_API);
+    let station = await fetch(`${PUBLIC_BACKEND_API}/stations/${params.station}`);
+    let dep_count = await fetch(`${PUBLIC_BACKEND_API}/stations/${params.station}/depatures/count`);
+    let dep_dist = await fetch(`${PUBLIC_BACKEND_API}/stations/${params.station}/depatures/distance`);
+    let dep_dur = await fetch(`${PUBLIC_BACKEND_API}/stations/${params.station}/depatures/duration`);
+    let ret_count = await fetch(`${PUBLIC_BACKEND_API}/stations/${params.station}/returns/count`);
 	return {
 			station: await station.json() as Station,
             depatures_count: (await dep_count.json()).count as number,
