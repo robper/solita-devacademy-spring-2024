8d311360a8aa46313c87e190b22b4d5361f6a477
Robin <paaarsson@gmail.com>
2024-07-01 21:54:21 +0200

    Import and instansiate the map component

---
+++ sda-frontend/src/routes/map/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Map from '$lib/components/Map.svelte';
+    import L from "leaflet";
+    export let data;
+    
+    let layers: L.Layer = L.marker([51.6, 7.5]);
+    let initPos: L.LatLngExpression = [51.514244, 7.468429]
+</script>
+
+<svelte:head>
+    <title>MapLibre GL JS</title>
+    <meta name="description" content="MapLibre GL JS" />
+</svelte:head>
+
+<Map view={initPos} zoom={10} layers={layers}/>
+
+<style></style>
