03c14a56f150188aeb7b9f318a14c32e409a27e5
Robin <paaarsson@gmail.com>
2024-07-03 20:52:12 +0200

    White background

--- sda-frontend/src/routes/+layout.svelte
+++ sda-frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <nav id="nav">
 	<a href="/">Home</a>
 	<a href="/stations">Stations</a>
+	<a href="/map">Map</a>
 </nav>
 <div id="remaining">
 	<slot />
@@ -9,11 +10,11 @@
 <style>
 	#nav {
 		height: 80px;
-		background-color: rgb(0, 255, 255);
+		background-color: rgb(161, 161, 161);
 	}
 
 	#remaining {
-		background-color: #6a9cff;
+		/* background-color: #6a9cff; */
 		height: calc(100vh - 80px);
 		width: 100vw;
 	}
