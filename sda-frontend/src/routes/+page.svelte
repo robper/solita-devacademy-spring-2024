7b89f3b5fa05245216ed6fa31a96d5b0a0daf359
Robin Persson <paaarsson@gmail.com>
2024-07-18 15:44:21 +0200

    Redirected and removed content from unused page

--- sda-frontend/src/routes/+page.svelte
+++ sda-frontend/src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<h1>Welcome</h1>
-<p>All stations view <a href="/stations">here</a></p>
-
 <svelte:head>
     <title>Home</title>
     <meta name="description" content="Home" />
---
+++ sda-frontend/src/routes/+page.ts
@@ -0,0 +1,4 @@
+import { redirect } from "@sveltejs/kit";
+export async function load({ }) {
+    redirect(301, "/map");
+}
