7c990e19a27e08448d83d3e8ba1e5aabd4e6f14e
Robin <paaarsson@gmail.com>
2024-07-28 19:20:09 +0200

    Error page

---
+++ sda-frontend/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h1>This is an error page for all errors on all routes</h1>
+<h2>Here's your error</h2>
+<p>{$page.error?.message}</p>
